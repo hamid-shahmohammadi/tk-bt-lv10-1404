542f91de0c3ad99958a2f8c0766a4ce9ec71515a
shah <hamid.shahmohamadi@gmail.com>
2026-01-27 17:41:46 +0330

    only active customer create harm

--- resources/views/filament/resources/customer-resource/pages/harm.blade.php
+++ resources/views/filament/resources/customer-resource/pages/harm.blade.php
@@ -1,6 +1,6 @@
 <x-filament-panels::page>
     <div class="flex justify-between">
-        @if (auth()->user()->can('create_customer'))
+        @if (auth()->user()->can('create_customer') && $customer->active)
             <x-modal.index title="ثبت خسارت">
                 <x-harm.form :customer="$customer" :message="$message" />
             </x-modal.index>
